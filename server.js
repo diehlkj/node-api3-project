90c3518a8434c323695bf6e2fa0340df956b4772
Ken Diehl <diehlkj.99@gmail.com>
2020-03-19 18:25:11 -0400

    Setting things up

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
 const server = express();
+const userRouter = require("./users/userRouter.js");
+const postRouter = require("./posts/postRouter.js");
+
+server.use(express.json());
+server.use("/api/post", postRouter);
+server.use("/api/user", userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
